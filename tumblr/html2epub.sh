8c71670ab33f7e84b0ad66ee47e29a530049642b
sebadorn <seba_dorn@gmx.de>
2016-06-05 12:13:13 +0200

    Minor changes.

--- tumblr/html2epub.sh
+++ tumblr/html2epub.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 AUTHOR='Baba Okina'
-COVER='cover.png'
+COVER='../covers/spider.png'
 TAGS='Fantasy,Light Novel'
 TITLE='I’m A Spider, So What?'
 
@@ -11,7 +11,14 @@ cd $(dirname "$0")
 
 cat downloads/*.html >> chapters.html
 
-ebook-convert chapters.html chapters.epub --chapter="//*[name()='h1' or name()='h2']" --extra-css="$CSS" --max-levels=0 --authors="$AUTHOR" --cover="$COVER" --tags="$TAGS" --title="$TITLE"
+ebook-convert chapters.html chapters.epub \
+	--chapter="//*[name()='h1' or name()='h2']" \
+	--language="en" \
+	--extra-css="$CSS" --max-levels=0 \
+	--authors="$AUTHOR" \
+	--cover="$COVER" --preserve-cover-aspect-ratio \
+	--tags="$TAGS" \
+	--title="$TITLE"
 
 rm chapters.html
 
